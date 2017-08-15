739fdc796751215549c8b8c768dca59e203f0489
Yun Ching Lim <yunching.lim@gmail.com>
2017-08-15 10:46:12 -0400

    Still trying to tidy data.

--- R/main.R
+++ R/main.R
@@ -12,15 +12,22 @@ ext_tracks_colnames <- c("storm_id", "storm_name", "month", "day",
                          paste("radius_64", c("ne", "se", "sw", "nw"), sep = "_"),
                          "storm_type", "distance_to_land", "final")
 
-ext_tracks <- read_fwf(file.path("G:","R projects", "hurricane_geom", "data", "ebtrk_atlc_1988_2015.txt"), 
+# ext_tracks <- read_fwf(file.path("G:","R projects", "hurricane_geom", "data", "ebtrk_atlc_1988_2015.txt"),
+#                        fwf_widths(ext_tracks_widths, ext_tracks_colnames),
+#                        na = "-99")
+ext_tracks <- read_fwf(file.path("~", "R projects", "hurricane_geom", "data", "ebtrk_atlc_1988_2015.txt"),
                        fwf_widths(ext_tracks_widths, ext_tracks_colnames),
                        na = "-99")
 
-tmp <- ext_tracks %>% 
-  gather(sector_speed, radii, 15:26)
+ext_tracks %>% 
+  select(storm_name, month, day, hour, year, latitude, longitude, starts_with("radius")) %>% 
+  gather(sector_speed, radii, starts_with("radius"), na.rm = TRUE) %>% 
   transmute(storm_id = paste(stringr::str_to_title(storm_name), year, sep = "-"), 
             date = ymd_h(paste(year, month, day, hour, sep = "-")),
-            lattitude = lattitude,
+            lattitude = latitude,
             longitude = longitude, 
-             )
+            sector_speed,
+            radii
+             ) %>% 
+  separate(sector_speed, c("soeed", "sector"), )
          
